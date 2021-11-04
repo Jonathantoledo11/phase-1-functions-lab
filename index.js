c0d20af837863bfd311f1dd30b0ec259e0573840
Jonathan <jonathantoledo11@gmail.com>
2021-11-04 10:36:00 -0700

    commit tree 20a22e0b059d375a1f8e5030ed605d42a15ebaaa

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks() {
+    return  8;   
+}
+
+
+function distanceFromHqInFeet() {
+    return 264;
+}
+
+function distanceFromHqInFeet() {
+    return 2112;
+}
+
+console.log(distanceFromHqInFeet());
+
+
+function distanceTravelledInFeet() {
+    return 1320;
+}
+
+function distanceTravelledInFeet() {
+    return 2640;
+
+}
+
+
